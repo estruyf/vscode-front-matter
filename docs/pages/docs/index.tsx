b04fdae043ecede2e1b77d306eec9b51a17e88c2
Elio Struyf <elio@struyfconsulting.be>
2021-08-30 18:10:14 +0200

    Added link for documentation

--- docs/pages/docs/index.tsx
+++ docs/pages/docs/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
 import { Description, OtherMeta, Title } from '../../components/Meta';
 import { Layout } from '../../components/Page/Layout';
+import { Extension } from '../../constants/extension';
 import { getAllPosts } from '../../lib/api';
+import { getPostByFilename } from '../../lib/api';
 
-export default function Home({ docs }: any) {
+export default function Home({ content }: any) {
   const { t: strings } = useTranslation();
   
   return (
@@ -15,8 +19,12 @@ export default function Home({ docs }: any) {
 
       <Layout>
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
-          <div className="text-6xl text-whisper-500">
-            Coming soon...
+          <div className="text-6xl text-whisper-500 text-center">
+            🚧 Working on it 🚧
+          </div>
+          
+          <div className="mt-8 text-lg">
+            The new documentation format is currently being created, but you can currently find the Front Matter documenation on the <a className={`text-teal-500 hover:text-teal-700`} href={Extension.githubLink}>GitHub repository</a>.
           </div>
         </div>
       </Layout>
