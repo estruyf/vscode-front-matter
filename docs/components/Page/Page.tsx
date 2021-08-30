596804ac79961266311e2892dd8f37a4afd38510
Elio Struyf <info@estruyf.be>
2021-08-30 09:44:48 +0200

    Docs

---
+++ docs/components/Page/Page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface IPageProps {}
+
+export const Page: React.FunctionComponent<IPageProps> = (props: React.PropsWithChildren<IPageProps>) => {
+  return (
+    <>
+      
+    </>
+  );
+};
---
+++ docs/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="mt-4 text-4xl tracking-tight font-extrabold sm:mt-5 sm:leading-none lg:mt-6 lg:text-5xl xl:text-6xl"><span className="md:block">Manage your static site with</span> <span className="text-teal-500 md:block">Front Matter</span></h1>
+  )
+}
+
+export default Home
