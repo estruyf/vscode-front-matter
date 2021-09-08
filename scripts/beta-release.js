b5033f6b454374880bd46670b5d80274e8c87adf
Elio Struyf <elio@struyfconsulting.be>
2021-09-08 08:39:24 +0200

    Beta release

---
+++ scripts/beta-release.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJson = require('../package.json');
+packageJson.version += `-beta.${process.argv[process.argv.length-1].substr(0, 7)}`;
+packageJson.preview = true;
+packageJson.name = `${packageJson.name}-beta`;
+packageJson.displayName = `${packageJson.displayName} BETA`;
+packageJson.description = `BETA Version of Front Matter. ${packageJson.description}`;
+packageJson.icon = "assets/frontmatter-beta.png";
+
+console.log(packageJson.version);
+
+fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
