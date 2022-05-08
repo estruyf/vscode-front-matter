426dbc2e46f36800170e20adc6981d2209e0f4b4
Elio Struyf <elio@struyfconsulting.be>
2022-05-08 20:02:35 +0200

    Update beta script

--- scripts/beta-release.js
+++ scripts/beta-release.js
@@ -31,5 +31,5 @@ fs.writeFileSync(path.join(__dirname, '../README.md'), readme);
 // Update the .vscodeignore file
 const ignoreFilePath = path.join(path.resolve('.'), '.vscodeignore');
 let vscodeignore = fs.readFileSync(ignoreFilePath, 'utf8');
-vscodeignore.replace(`**/*.map`, '');
+vscodeignore = vscodeignore.replace(`**/*.map`, '');
 fs.writeFileSync(ignoreFilePath, vscodeignore);
