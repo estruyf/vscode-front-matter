b48008eb4143826f6cade4980325f135f1676192
Elio Struyf <elio@struyfconsulting.be>
2023-03-16 09:30:05 +0100

    #537 - added support for `./` in public folder

--- src/commands/Folders.ts
+++ src/commands/Folders.ts
@@ -174,9 +174,14 @@ export class Folders {
   public static getStaticFolderRelativePath(): string | undefined {
     let staticFolder = Settings.get<string>(SETTING_CONTENT_STATIC_FOLDER);
 
-    if (staticFolder && (staticFolder.includes(WORKSPACE_PLACEHOLDER) || staticFolder === '/')) {
+    if (
+      staticFolder &&
+      (staticFolder.includes(WORKSPACE_PLACEHOLDER) ||
+        staticFolder === '/' ||
+        staticFolder === './')
+    ) {
       staticFolder =
-        staticFolder === '/'
+        staticFolder === '/' || staticFolder === './'
           ? Folders.getAbsFilePath('[[workspace]]')
           : Folders.getAbsFilePath(staticFolder);
       const wsFolder = Folders.getWorkspaceFolder();
