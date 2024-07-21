380bc804fdcae6b339f7235f5e55d53c9b6fa072
Elio Struyf <elio@struyfconsulting.be>
2024-07-21 20:37:43 +0200

    Update beta script

--- scripts/beta-release.js
+++ scripts/beta-release.js
@@ -5,13 +5,13 @@ const core = require('@actions/core');
 const packageJson = require('../package.json');
 const version = packageJson.version.split('.');
 
-packageJson.version = `${version[0]}.${version[1]}.${process.argv[process.argv.length-1].substr(0, 7)}`;
+packageJson.version = `${version[0]}.${version[1]}.${process.argv[process.argv.length - 1]}`;
 packageJson.preview = true;
-packageJson.name = "vscode-front-matter-beta";
+packageJson.name = 'vscode-front-matter-beta';
 packageJson.displayName = `${packageJson.displayName} (BETA)`;
 packageJson.description = `BETA Version of Front Matter. ${packageJson.description}`;
-packageJson.icon = "assets/frontmatter-beta.png";
-packageJson.homepage = "https://beta.frontmatter.codes";
+packageJson.icon = 'assets/frontmatter-beta.png';
+packageJson.homepage = 'https://beta.frontmatter.codes';
 
 console.log(packageJson.version);
 
@@ -20,13 +20,16 @@ core.summary.addHeading(`Version info`).addRaw(`Version: ${packageJson.version}`
 const scripts = packageJson.scripts;
 for (const key in scripts) {
   if (key.startsWith(`prod:`)) {
-    scripts[key] = scripts[key].replace("production", "development");
+    scripts[key] = scripts[key].replace('production', 'development');
   }
 }
 
 console.log(JSON.stringify(packageJson.scripts, null, 2));
 
-fs.writeFileSync(path.join(path.resolve('.'), 'package.json'), JSON.stringify(packageJson, null, 2));
+fs.writeFileSync(
+  path.join(path.resolve('.'), 'package.json'),
+  JSON.stringify(packageJson, null, 2)
+);
 
 let readme = fs.readFileSync(path.join(__dirname, '../README.beta.md'), 'utf8');
 fs.writeFileSync(path.join(__dirname, '../README.md'), readme);
